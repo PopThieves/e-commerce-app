056f6cacdc2e944c42086cabe240f486a91a805b
Sam <samblackburn2828@gmail.com>
2021-02-02 13:24:31 +0000

    added navbar with logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,22 +9,33 @@ import {
   Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
-import classes from "*.module.css";
+
+import logo from "../../assets/logo.png";
+import useStyles from "./styles";
 
 const Navbar = () => {
+  const classes = useStyles();
   return (
     <div>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography variant="h6" className={classes.title} color="inherit">
             <img
-              src={}
+              src={logo}
               alt="commerce.js"
               height="25px"
               className={classes.image}
             />
             Commerce.js
           </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show Cart Items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
