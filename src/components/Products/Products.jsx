63e8d7736c1395d574176e648cbcaf652ba61021
Sam <samblackburn2828@gmail.com>
2021-02-07 15:08:06 +0000

    started the cart component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,26 +4,7 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Headset",
-    description: "Gaming Headset",
-    price: "£50",
-    image:
-      "https://legitreviews.com/wp-content/uploads/2017/01/Logitech-G533_Carmen_BTY3.jpg",
-  },
-  {
-    id: 2,
-    name: "Shoes",
-    description: "Running Shoes",
-    price: "£30",
-    image:
-      "https://res.cloudinary.com/eastern/image/upload/w_1000,q_auto,f_auto/2046070_089_main.jpg",
-  },
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
@@ -31,7 +12,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
